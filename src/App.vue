d0595170379389bb608ba61de22aece2ead0fd44
Haoran Zhang <21968907+zhr1996@users.noreply.github.com>
2021-06-20 17:20:53 -0400

    set up vue app

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">  
+    <h1>To-Do List</h1> 
+    <ul>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item :label="item.label" :done="item.done"></to-do-item>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from "./components/ToDoItem.vue"
+import uniqueId from 'lodash.uniqueid';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDoItem,
+  },
+  data() {
+    return {
+      ToDoItems: [
+        { id: uniqueId('todo-'), label: 'Learn Vue', done: false },
+        { id: uniqueId('todo-'), label: 'Create a Vue project with the CLI', done: true },
+        { id: uniqueId('todo-'), label: 'Have fun', done: true },
+        { id: uniqueId('todo-'), label: 'Create a to-do list', done: false }
+      ]
+    };
   }
-}
+};
+
 </script>
 
 <style>
