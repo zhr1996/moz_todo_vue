d21c732ea3c986e949b9c34ba00861765cfa0c50
Haoran Zhang <21968907+zhr1996@users.noreply.github.com>
2021-06-20 21:27:07 -0400

    add interactive ?

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,14 @@
   <div id="app">  
     <h1>To-Do List</h1>
     <to-do-form @todo-added="addToDo"></to-do-form>
+    <h2 id="list-summary">{{listSummary}}</h2>
     <ul aria-labelledby="list-summary" class="stack-large">
       <li v-for="item in ToDoItems" :key="item.id">
-        <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
+        <to-do-item :label="item.label" :done="item.done" :id="item.id"
+                    @checkbox-changed="updateDoneStatus(item.id)"
+                    @item-deleted="deleteToDo(item.id)"
+                    @item-edited="editToDo(item.id, $event)">
+        </to-do-item>      
       </li>
     </ul>
   </div>
@@ -34,6 +39,24 @@ export default {
     methods: {
       addToDo(toDoLabel) {
         this.ToDoItems.push({id:uniqueId('todo-'), label: toDoLabel, done: false});
+      },
+      updateDoneStatus(toDoId) {
+        const toDoToUpdate = this.ToDoItems.find(item => item.id === toDoId)
+        toDoToUpdate.done = !toDoToUpdate.done
+      },
+      deleteToDo(toDoId) {
+        const itemIndex = this.ToDoItems.findIndex(item => item.id === toDoId);
+        this.ToDoItems.splice(itemIndex, 1);
+      },
+      editToDo(toDoId, newLabel) {
+        const toDoToEdit = this.ToDoItems.find(item => item.id === toDoId);
+        toDoToEdit.label = newLabel;
+      }
+    },
+    computed: {
+      listSummary() {
+        const numberFinishedItems = this.ToDoItems.filter(item =>item.done).length
+        return `${numberFinishedItems} out of ${this.ToDoItems.length} items completed`
       }
     }
   };
--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,17 +1,35 @@
 
 
+
 <template>
+  <div class="stack-small" v-if="!isEditing">
     <div class="custom-checkbox">
-        <input class="checkbox" type="checkbox" :id="id" :checked="isDone" />
-        <label class="checkbox-label" :for="id">{{label}}</label>
+      <input type="checkbox" class="checkbox" :id="id" :checked="isDone"
+             @change="$emit('checkbox-changed')" />
+      <label :for="id" class="checkbox-label">{{label}}</label>
     </div>
+    <div class="btn-group">
+      <button type="button" class="btn"  @click="toggleToItemEditForm">
+        Edit <span class="visually-hidden">{{label}}</span>
+      </button>
+      <button type="button" class="btn btn__danger" @click="deleteToDo">
+        Delete <span class="visually-hidden">{{label}}</span>
+      </button>
+    </div>
+  </div>
+  
+  <to-do-item-edit-form v-else :id="id" :label="label"
+                      @item-edited="itemEdited"
+                      @edit-cancelled="editCancelled">
+</to-do-item-edit-form>
 </template>
 
-
-
 <script>
-
+    import ToDoItemEditForm from "./ToDoItemEditForm";
     export default {
+        components: {
+            ToDoItemEditForm
+        },
         props: {
             label: {required: true, type: String},
             done: {default:false, type: Boolean},
@@ -19,8 +37,28 @@
         },
         data() {
             return {
-                isDone: this.done,
+                isEditing: false
             };
+        },
+        computed: {
+            isDone() {
+                return this.done;
+        }
+        },
+        methods: {
+            deleteToDo() {
+                this.$emit('item-deleted');
+            },
+            toggleToItemEditForm() {
+                this.isEditing = true;
+            },
+            itemEdited(newLabel) {
+                this.$emit('item-edited', newLabel);
+                this.isEditing = false;
+            },
+            editCancelled() {
+                this.isEditing = false;
+            }
         }   
 
     };
@@ -28,17 +66,17 @@
 
 <style scoped>
     .custom-checkbox > .checkbox-label {
-  font-family: Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  font-weight: 400;
-  font-size: 16px;
-  font-size: 1rem;
-  line-height: 1.25;
-  color: #0b0c0c;
-  display: block;
-  margin-bottom: 5px;
-}
+        font-family: Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        font-weight: 400;
+        font-size: 16px;
+        font-size: 1rem;
+        line-height: 1.25;
+        color: #0b0c0c;
+        display: block;
+        margin-bottom: 5px;
+    }
 .custom-checkbox > .checkbox {
   font-family: Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
